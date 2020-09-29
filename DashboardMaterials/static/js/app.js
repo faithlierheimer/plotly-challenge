a1b8ea33a573e55b02c724781e3f027d653925df
faithlierheimer <flierheimer@gmail.com>
2020-09-29 13:10:19 -0600

    The bar lengths are thicker now

--- DashboardMaterials/static/js/app.js
+++ DashboardMaterials/static/js/app.js
@@ -4,7 +4,22 @@ d3.json("../../samples.json").then((data)=>{
     var otu_ids = data.samples.map(d => d.otu_ids[0]);
     var sample_values = data.samples.map(d => d.sample_values[0])
     var wfreq = data.metadata.map(d => d.wfreq);
-    console.log(sample_values);
+    var trace1 = {
+        type: "bar",
+        name: "test",
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        width: 100
+    };
+    var layout = {
+        title: `OTU Frequency`,
+        width: 1000,
+        height: 1000
+
+    };
+    var data = [trace1];
+    Plotly.newPlot("bar", data, layout);
 });
 
 
